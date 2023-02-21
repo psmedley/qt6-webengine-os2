4c70325abdb7e6c9abd9c96286d0dc08724236d0
Paul Smedley <paul@smedley.id.au>
2023-02-22 09:17:30 +1030

    Further improvements to openh264 asm
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- src/3rdparty/chromium/third_party/openh264/src/codec/encoder/core/x86/coeff.asm
+++ src/3rdparty/chromium/third_party/openh264/src/codec/encoder/core/x86/coeff.asm
@@ -44,6 +44,10 @@
 
 %ifdef X86_32_PICASM
 SECTION .text align=16
+%elifidn __OUTPUT_FORMAT__,obj
+        ; OMF needs special handling to ensure everything is in the same segment
+        ; and that the segment is 32 bit.
+        SECTION TEXT32 align=16 public use32 class=CODE
 %else
 SECTION .rodata align=16
 %endif
